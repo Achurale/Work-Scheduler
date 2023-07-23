ad777ac0ace56611a42e997c79f69338fb00d1fb
Roymand <chuyirlee@live.com>
2023-07-22 20:47:32 -0500

    Removed starter comments and included my own comments on functionality in js code

--- Assets/script.js
+++ Assets/script.js
@@ -1,26 +1,17 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-// var currentDate = dayjs(undefined, 'MM-DD-YYYY')
-// console.log(currentDate)
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
 
+  // Clicking on a save button will save its corresponding text box into local storage
+  // using the id of its parent as a key
   $('.saveBtn').click(function() {
     localStorage.setItem($(this).parent().attr('id'), $(this).parent().children('.description').val())
   })
 
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // Each hour will change color on load in depending on whether it is in the past,
+  // present, or the future of the current hour of the day
     if (dayjs().hour(9).isBefore(dayjs())){
     $('#hour-9').addClass('past')
   } else if (dayjs().hour(9).isSame(dayjs())) {
@@ -93,9 +84,8 @@ $(document).ready(function () {
     $('#hour-5').addClass('future')
   }
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // The text box of every hour will load in with text according to what
+  // the user has previously saved in their respective slots
   
   $('#text-9').text(localStorage.getItem('hour-9'))
   $('#text-10').text(localStorage.getItem('hour-10'))
@@ -107,6 +97,6 @@ $(document).ready(function () {
   $('#text-4').text(localStorage.getItem('hour-4'))
   $('#text-5').text(localStorage.getItem('hour-5'))
 
-  // TODO: Add code to display the current date in the header of the page.
+  // The current date will be displayed at the top of the page
   $('#currentDay').text(dayjs().format("MM-DD-YYYY"))
 });
